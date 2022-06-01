17d8112731207a28cb39ec9cb5249383d18ebc07
tlopez <tlopez@poderjudicial.gob.hn>
2022-06-01 14:26:36 -0600

    test deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static('./dist/sumadiStudentsApp/'));
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
     res.sendFile('./dist/sumadiStudentsApp/index.html');
 });
 app.listen(process.env.PORT || 8080);
