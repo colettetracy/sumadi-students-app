8dc0899c219e89e2248959e0df87a3b5457d9468
tlopez <tlopez@poderjudicial.gob.hn>
2022-06-01 14:12:22 -0600

    test 2 error refresh

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use('/', express.static('./dist/sumadiStudentsApp/'));
-app.use('*', function(req, res) {
+app.use(express.static('./dist/sumadiStudentsApp/'));
+app.get('/*', function(req, res) {
     res.sendFile('./dist/sumadiStudentsApp/index.html');
 });
 app.listen(process.env.PORT || 8080);
