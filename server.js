8bc0c301a2d109af0a7bbd2ec55bcf3d4806c6f8
tlopez <tlopez@poderjudicial.gob.hn>
2022-06-01 12:49:51 -0600

    config updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/sumadiStudentsApp/'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/' });
+    res.sendFile('./dist/sumadiStudentsApp/index.html');
 });
 app.listen(process.env.PORT || 8080);
