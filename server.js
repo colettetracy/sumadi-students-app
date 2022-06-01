e8dfa5155c664fa94c82a4e4147f276f5f4aa66c
tlopez <tlopez@poderjudicial.gob.hn>
2022-06-01 12:44:27 -0600

    server.js edited

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<sumadiStudentsApp>'));
+app.use(express.static('./dist/'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/<sumadiStudentsApp>/index.html'));
+    res.sendFile('index.html', { root: 'dist/' });
 });
 app.listen(process.env.PORT || 8080);
